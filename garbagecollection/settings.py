8ff876fa175b9e99f6e530f5d3a00c76b00833a7
nrbrt2002 <nrbrt2002#gmail.com>
2024-11-09 14:45:03 +0400

    new things

--- garbagecollection/settings.py
+++ garbagecollection/settings.py
@@ -144,7 +144,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'normuyomba@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_APP_PASSWORD')
+EMAIL_HOST_PASSWORD = 'xnkn ccql oojm sxdj'
 DEFAULT_FROM_EMAIL = 'normuyomba@gmail.com'
 
 
